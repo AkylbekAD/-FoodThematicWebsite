82b883da73e0679e800bb133b62065349f0b846e
Akylbek <Abdykerimov.Akylbek@yandex.ru>
2021-11-15 16:21:15 +0700

    Second commit, XML updated to fetch

--- js/script.js
+++ js/script.js
@@ -294,5 +294,15 @@ window.addEventListener('DOMContentLoaded', () => {
         closeModal();
       }, 4000);
     }
+
+    // fetch('https://jsonplaceholder.typicode.com/todos/1', {
+    //   method: 'POST',
+    //   body: JSON.stringify({ name: 'Alex' }),
+    //   headers: {
+    //     'Content-type': 'application/json',
+    //   },
+    // })
+    //   .then((response) => response.json())
+    //   .then((json) => console.log(json));
   }
 });
