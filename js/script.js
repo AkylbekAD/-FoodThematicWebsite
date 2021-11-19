ac05e1328bc0aea13d112320c9dc161687750a77
Akylbek <Abdykerimov.Akylbek@yandex.ru>
2021-11-19 18:46:49 +0700

    Third commit: .gitignore, package-lock.json, package.json, db.json and POST and DB reading updated

--- js/script.js
+++ js/script.js
@@ -156,6 +156,7 @@ window.addEventListener('DOMContentLoaded', () => {
       this.classes = classes;
       this.changeToRuble();
       this.parent = document.querySelector(parent);
+      this.renderMenuItem();
     }
 
     changeToRuble() {
@@ -182,41 +183,21 @@ window.addEventListener('DOMContentLoaded', () => {
     }
   }
 
-  new MenuItem(
-    'img/tabs/hamburger.jpg',
-    'burger',
-    'Американо',
-    'Для жирных уебкаффф',
-    2,
-    '.menu .container'
-  ).renderMenuItem();
+  const getResource = async (url) => {
+    let result = await fetch(url);
 
-  // new MenuItem(
-  //   'img/tabs/vegy.jpg',
-  //   'vegy',
-  //   'Меню "Фитнес"',
-  //   'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-  //   9,
-  //   '.menu .container'
-  // ).renderMenuItem();
+    if (!result.ok) {
+      throw new Error(`Could not fetch ${url}, status: ${result.status}`);
+    }
 
-  new MenuItem(
-    'img/tabs/post.jpg',
-    'post',
-    'Меню "Постное"',
-    'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-    14,
-    '.menu .container'
-  ).renderMenuItem();
+    return await result.json();
+  };
 
-  new MenuItem(
-    'img/tabs/elite.jpg',
-    'elite',
-    'Меню “Премиум”',
-    'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-    21,
-    '.menu .container'
-  ).renderMenuItem();
+  getResource('http://localhost:3000/menu').then((data) => {
+    data.forEach(({ img, alt, title, descr, price, parent }) => {
+      new MenuItem(img, alt, title, descr, price, parent);
+    });
+  });
 
   const forms = document.querySelectorAll('form');
 
@@ -227,10 +208,21 @@ window.addEventListener('DOMContentLoaded', () => {
   };
 
   forms.forEach((item) => {
-    postData(item);
+    bindPostData(item);
   });
 
-  function postData(form) {
+  const postData = async (url, data) => {
+    let result = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: data,
+    });
+    return await result.json();
+  };
+
+  function bindPostData(form) {
     form.addEventListener('submit', (e) => {
       e.preventDefault();
 
@@ -243,20 +235,14 @@ window.addEventListener('DOMContentLoaded', () => {
       form.insertAdjacentElement('afterend', statusMessage);
 
       const formData = new FormData(form);
+      // const object = {};
+      // formData.forEach(function (value, key) {
+      //   object[key] = value;
+      // });
 
-      const object = {};
-      formData.forEach(function (value, key) {
-        object[key] = value;
-      });
+      let json = JSON.stringify(Object.fromEntries(formData.entries()));
 
-      fetch('server.php', {
-        method: 'POST',
-        headers: {
-          'Content-type': 'application/json',
-        },
-        body: JSON.stringify(object),
-      })
-        .then((data) => data.text())
+      postData('http://localhost:3000/requests', json)
         .then((data) => {
           console.log(data);
           showThanksModal(message.success);
@@ -294,15 +280,9 @@ window.addEventListener('DOMContentLoaded', () => {
         closeModal();
       }, 4000);
     }
-
-    // fetch('https://jsonplaceholder.typicode.com/todos/1', {
-    //   method: 'POST',
-    //   body: JSON.stringify({ name: 'Alex' }),
-    //   headers: {
-    //     'Content-type': 'application/json',
-    //   },
-    // })
-    //   .then((response) => response.json())
-    //   .then((json) => console.log(json));
   }
+
+  // fetch('http://localhost:3000/menu')
+  //   .then((data) => data.json())
+  //   .then((res) => console.log(res));
 });
